c8bcb82ce57f83c0409319b02a49c4e9b9260301
trosemberg <tomasroro@hotmail.com>
2020-08-05 12:32:04 -0300

    comments on files and format of datetime.now

--- application.py
+++ application.py
@@ -11,7 +11,9 @@ from matplotlib.figure import Figure
 import seaborn as sns
 import datetime as dt
 
+# class Window is full application
 class Window:
+    # starts setting size and configs os app 
     def __init__(self,master):
         self.master = master
         self.WIDTH = 600
@@ -23,7 +25,7 @@ class Window:
         self.interval_dates = tk.IntVar()
         self.sv_date1 = tk.StringVar()
 
-    
+    # create upper frame with background image
     def set_frame_upper(self):
         self.frame_upper = tk.Frame(self.master)
         self.frame_upper.place(relwidth=1,relheight=0.2)
@@ -32,16 +34,18 @@ class Window:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image)
         self.canvas.pack()
 
-
+    # set lower frame with background red
     def set_frame_lower(self):
         self.frame_lower = tk.Frame(self.master,bg='red')
         self.frame_lower.place(rely = 0.2,relwidth=1,relheight=0.8)
 
+    # set label with title to upper frame
     def set_title(self):
         titulo_str = "Pesquisar Preço Dólar, data DDMMAAAA"
         self.titulo = tk.Label(self.frame_upper,text = titulo_str)
         self.titulo.place(relx = 0.5,anchor = tk.N)
 
+    # create first date entry
     def first_date_entry(self):
         self.sv_date1.trace("w",lambda name, index, mode, sv=self.sv_date1: self.entryUpdateDate(self.sv_date1))
         self.first_date = tk.Entry(self.frame_upper,
@@ -52,6 +56,7 @@ class Window:
             relheight=0.25)
         self.first_date.insert(0,'DD-MM-AAAA')
     
+    #create last_date entry
     def last_date_entry(self):
         self.sv_date2 = tk.StringVar()
         self.sv_date2.trace("w",lambda name, index, mode, sv=self.sv_date2: self.entryUpdateDate(self.sv_date2))
@@ -64,7 +69,7 @@ class Window:
             relheight=0.25)
         self.last_date.configure(state="disabled")
         
-
+    # functions to track changes on entrys and keep them on right format
     def entryUpdateDate(self,stringvar):
         digits = list(filter(str.isdigit,stringvar.get()))
         digits =  digits + ["0","0","0","0","0","0","0","0"]
@@ -73,21 +78,21 @@ class Window:
         year = int(digits[4]+digits[5]+digits[6]+digits[7])
         stringvar.set("{:02d}{:02d}{:04d}".format(day,month,year))
 
-
+    # function that enable or disable second entry
     def second_date_use(self):
         if self.interval_dates.get()==1:
             self.last_date.configure(state="normal")
             self.last_date.update()
             self.last_date.delete(0,'end')
             
-            self.last_date.insert(0,dt.datetime.now().strftime("%m%d%Y"))
+            self.last_date.insert(0,dt.datetime.now().strftime("%d%m%Y"))
         else:
             self.last_date.update()
             self.last_date.delete(0,'end')
             self.last_date.insert(0,'DD-MM-AAAA')
             self.last_date.configure(state="disabled")
 
-
+    # create checkbox to enable/disable second entry
     def set_checkbox_last_date(self):
         self.checkbox_last_date = tk.Checkbutton(
             self.frame_upper, text = "Dólar em um Periodo",
@@ -97,6 +102,7 @@ class Window:
         self.checkbox_last_date.place(relx = 0.7,
             rely = 0.50)
 
+    # create button that insert on first date entry the day plano Real started  
     def set_btn_begin_real(self):
         self.btn_begin_real = tk.Button(
             self.frame_upper,text = "Plano Real",
@@ -108,10 +114,12 @@ class Window:
             rely = 0.25, relwidth = 0.25, relheight=0.25
         )
     
+    # command used from begin real button 
     def set_real_date(self):
         self.first_date.delete(0,"end")
         self.first_date.insert(0,"01071994")
 
+    # crete search button
     def set_btn_search(self):
         self.btn_search = tk.Button(self.frame_upper,
             text='Pesquisar',
@@ -122,7 +130,7 @@ class Window:
             rely = 0.75,
             relwidth = 0.25,
             relheight=0.25)
-
+    # keeps date formated to keeps years, month and day on real ranger
     def format_date_numbers(self,date):
         day = int(date[0]+date[1])
         month = int(date[2]+date[3])
@@ -141,12 +149,14 @@ class Window:
             year=1984
         return "{:02d}{:02d}{:02d}".format(day,month,year)
 
+    # format date from entry to API format
     def format_date_to_request(self,date):
         day = int(date[0]+date[1])
         month = int(date[2]+date[3])
         year = int(date[4]+date[5]+date[6]+date[7])
         return "{:02d}-{:02d}-{:02d}".format(month,day,year)
 
+    # search command when search button is pressed
     def search(self):
         date1 = self.sv_date1.get()
         self.first_date.insert(0,self.format_date_numbers(date1))
@@ -155,6 +165,7 @@ class Window:
             self.last_date.insert(0,self.format_date_numbers(date2))
         self.request_api()
 
+    # clean objects in lower frame
     def clean_lower_frame(self):
         try:
             self.label_result.destroy()
@@ -169,8 +180,7 @@ class Window:
         except:
             pass
 
-
-
+    # request to api and manipulate data
     def request_api(self):
         if self.interval_dates.get() == 0:
             url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='{}'&$top=100&$format=json&$select=cotacaoCompra"
@@ -200,13 +210,14 @@ class Window:
                 dolar_comeco = data.iloc[0]['cotacaoCompra']
                 dolar_fim = data.iloc[-1]['cotacaoCompra']
                 dolar_var = (dolar_fim/dolar_comeco)
-                str = "Variação do Dólar = {:.3f}%\nComeço = {} | Fim = {}".format(dolar_var,dolar_comeco,dolar_fim)
+                str = "Variação do Dólar = {:,.3f}%\nComeço = {} | Fim = {}".format(dolar_var*100,dolar_comeco,dolar_fim)
             self.label_result = tk.Label(self.frame_lower,
             text = str)
             self.label_result.place(relx = 0.5,
             anchor=tk.N)
             self.create_graph(data)
 
+    # create graphs
     def create_graph(self,data):
         f = Figure(figsize=(5,5), dpi=100)
         a = f.add_subplot(111)
--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import requests
 
 
 def main():
+    # start aplication
     root = tk.Tk()
+    # create window 
     app = Window(root)
+    # create upper frame 
     app.set_frame_upper()
+    # create lower frame
     app.set_frame_lower()
+    # add to upper frame text with info
     app.set_title()
+    # create entry for first date on upper frame
     app.first_date_entry()
+    # create button to insert begin of "Plano Real" date on first entry on upper frame
     app.set_btn_begin_real()
+    # create last date entry on upper frame
     app.last_date_entry()
+    # create checkbox to enable last date entry on upper frame
     app.set_checkbox_last_date()
+    # create  search button on upper frame
     app.set_btn_search()
-
+    # exec mainloop
     root.mainloop()
 
 if __name__ == '__main__':
